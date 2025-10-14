cf9a6a3276a50f305eb661befc9255c7b5cfdeaa
xores1331 <xores1331@gmail.com>
2025-10-14 19:12:24 +0200

    Lab02-PUG-Hotfix

--- Lab2_0/app.js
+++ Lab2_0/app.js
@@ -6,7 +6,7 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.use(express.urlencoded({extended: false}));
@@ -16,7 +16,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404.pug', { pageTitle: 'Page Not Found', path: '/' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/' });
 });
 
 app.listen(33333);
